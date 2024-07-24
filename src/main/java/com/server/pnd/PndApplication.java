267e3ce5b2fb15d44887c5ad067a4f36fb0f1159
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-07-24 11:27:51 +0900

    🎉 Begin a project

---
+++ src/main/java/com/server/pnd/PndApplication.java
@@ -0,0 +1,13 @@
+package com.server.pnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/pnd/PndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.pnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
