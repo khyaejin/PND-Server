41449ff13397931889ed9496a0ab18dabcaadb35
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-01 03:44:50 +0900

    🔧 [Settings] BaseEntity 수정 - LocatDateTime에는 @NotEmpty 사용하지 않음

--- src/main/java/com/server/pnd/util/entity/BaseEntity.java
+++ src/main/java/com/server/pnd/util/entity/BaseEntity.java
@@ -10,6 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -17,10 +18,16 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
-    @NotEmpty @Column(name = "created_at")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @NotEmpty @Column(name = "updated_at")
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    // locatlDateTime -> String 형식으로 바꿔주는 메서드
+    public String localDateTimeToString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        return createdAt.format(formatter);
+    }
 }
