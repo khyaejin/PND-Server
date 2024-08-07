690a8808862fb118deb5b1b125c743f36d470cac
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-08 01:54:08 +0900

    ✨ [Feat] 프로젝트 생성 - 회원정보 찾을 수 없음 (404)

--- src/main/java/com/server/pnd/project/controller/ProjectController.java
+++ src/main/java/com/server/pnd/project/controller/ProjectController.java
@@ -1,11 +1,23 @@
 package com.server.pnd.project.controller;
 
+import com.server.pnd.markdown.dto.MarkdownSavedRequestDto;
+import com.server.pnd.project.dto.ProjectCreatedRequestDto;
+import com.server.pnd.project.service.ProjectService;
+import com.server.pnd.util.response.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/pnd/project")
 public class ProjectController {
+    private final ProjectService projectService;
+    // 프로젝트 생성
+    @PostMapping
+    public ResponseEntity<CustomApiResponse<?>> createProject(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody ProjectCreatedRequestDto projectCreatedRequestDto) {
+        return projectService.createProject(authorizationHeader, projectCreatedRequestDto);
+    }
 }
--- src/main/java/com/server/pnd/project/service/ProjectServiceImpl.java
+++ src/main/java/com/server/pnd/project/service/ProjectServiceImpl.java
@@ -1,9 +1,28 @@
 package com.server.pnd.project.service;
 
+import com.server.pnd.domain.User;
+import com.server.pnd.project.dto.ProjectCreatedRequestDto;
+import com.server.pnd.util.response.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService{
+    @Override
+    public ResponseEntity<CustomApiResponse<?>> createProject(String authorizationHeader, ProjectCreatedRequestDto projectCreatedRequestDto) {
+        Optional<User> foundUser = jwtUtil.findUserByJwtToken(authorizationHeader);
+
+        // 토큰에 해당하는 유저가 없는 경우 : 404
+        if (foundUser.isEmpty()) {
+            CustomApiResponse<?> res = CustomApiResponse.createFailWithoutData(404, "유효하지 않은 토큰이거나, 해당 ID에 해당하는 사용자가 존재하지 않습니다.");
+            return ResponseEntity.status(404).body(res);
+        }
+        User user = foundUser.get();
+
+        return null;
+    }
 }
