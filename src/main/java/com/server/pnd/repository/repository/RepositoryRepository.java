27fd2a3711fb2a85e51c60f1110218c8c6ae05bf
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-07 19:33:23 +0900

    ✨ [Feat] Repository repository 생성

--- src/main/java/com/server/pnd/repository/repository/RepositoryRepository.java
+++ src/main/java/com/server/pnd/repository/repository/RepositoryRepository.java
@@ -1,8 +1,12 @@
 package com.server.pnd.repository.repository;
 
 import com.server.pnd.domain.Repository;
+import com.server.pnd.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 @org.springframework.stereotype.Repository
 public interface RepositoryRepository extends JpaRepository<Repository, Long> {
+    Optional<Repository> findByRepositoryId(String repositoryId);
 }
