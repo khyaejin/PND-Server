8d4914d56d7613946558e83d4e7031088212dff1
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-07 23:36:41 +0900

    ✨ [Feat] 마크다운 문서 저장 - controller

--- src/main/java/com/server/pnd/markdown/service/MarkdownServiceImpl.java
+++ src/main/java/com/server/pnd/markdown/service/MarkdownServiceImpl.java
@@ -1,9 +1,18 @@
 package com.server.pnd.markdown.service;
 
+import com.server.pnd.markdown.dto.MarkdownSavedRequestDto;
+import com.server.pnd.util.response.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MarkdownServiceImpl implements MarkdownService{
+
+    // 마크다운 문서 저장
+    @Override
+    public ResponseEntity<CustomApiResponse<?>> savedMarkdown(String authorizationHeader, MarkdownSavedRequestDto markdownSavedRequestDto) {
+        return null;
+    }
 }
