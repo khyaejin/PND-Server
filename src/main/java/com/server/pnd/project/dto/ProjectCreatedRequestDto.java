3786b7ff497af740e4075ea32423d46de07c5839
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-08 01:47:46 +0900

    ✨ [Feat] 프로젝트 생성 - request dto 생성

---
+++ src/main/java/com/server/pnd/project/dto/ProjectCreatedRequestDto.java
@@ -0,0 +1,17 @@
+package com.server.pnd.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class ProjectCreatedRequestDto {
+    private Long repositoryId;
+    private String period;
+    private String image;
+    private String part;
+}
