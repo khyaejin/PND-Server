7e364519828b88bcd27cc3bc1ec9305b801f3bea
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-08 01:59:06 +0900

    ✨ [Feat] 프로젝트 생성 - 해당하는 ID를 가진 프로젝트가 없는 경우 (404)

--- src/main/java/com/server/pnd/project/service/ProjectServiceImpl.java
+++ src/main/java/com/server/pnd/project/service/ProjectServiceImpl.java
@@ -1,7 +1,10 @@
 package com.server.pnd.project.service;
 
+import com.server.pnd.domain.Repository;
 import com.server.pnd.domain.User;
 import com.server.pnd.project.dto.ProjectCreatedRequestDto;
+import com.server.pnd.repository.repository.RepositoryRepository;
+import com.server.pnd.util.jwt.JwtUtil;
 import com.server.pnd.util.response.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService{
+    private final JwtUtil jwtUtil;
+    private final RepositoryRepository repositoryRepository;
+
     @Override
     public ResponseEntity<CustomApiResponse<?>> createProject(String authorizationHeader, ProjectCreatedRequestDto projectCreatedRequestDto) {
         Optional<User> foundUser = jwtUtil.findUserByJwtToken(authorizationHeader);
@@ -22,6 +28,14 @@ public class ProjectServiceImpl implements ProjectService{
             return ResponseEntity.status(404).body(res);
         }
         User user = foundUser.get();
+
+        Optional<Repository> foundRepository = repositoryRepository.findById(projectCreatedRequestDto.getRepositoryId());
+        // 해당 Id에 해당하는 레포가 없는 경우 : 404
+        if (foundRepository.isEmpty()) {
+            CustomApiResponse<?> res = CustomApiResponse.createFailWithoutData(404, "해당 ID를 가진 레포지토리가 존재하지 않습니다.");
+            return ResponseEntity.status(404).body(res);
+        }
+
 
         return null;
     }
