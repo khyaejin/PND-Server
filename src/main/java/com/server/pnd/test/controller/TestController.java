e91d78aa06e0682850db73ad0639d050e1c5fdea
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-09 04:02:55 +0900

    ✨ [Feat] 클래스다이어그램 생성 테스트 api - dto 생성

--- src/main/java/com/server/pnd/test/controller/TestController.java
+++ src/main/java/com/server/pnd/test/controller/TestController.java
@@ -1,12 +1,26 @@
 package com.server.pnd.test.controller;
 
+import com.server.pnd.project.dto.ProjectCreatedRequestDto;
+import com.server.pnd.test.service.TestService;
+import com.server.pnd.util.response.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
-    // 클래스 다이어그램 생성 테스트 API
+    private final TestService testService;
 
+    // 클래스 다이어그램 생성 테스트 API
+    @PostMapping
+    public ResponseEntity<CustomApiResponse<?>> createProject(
+            @RequestBody ClassDiagramCreatedRequestDto classDiagramCreatedRequestDto) {
+        return testService.createClassDiagram(classDiagramCreatedRequestDto);
+    }
 }
---
+++ src/main/java/com/server/pnd/test/dto/ClassDiagramCreatedRequestDto.java
@@ -0,0 +1,12 @@
+package com.server.pnd.test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@Builder
+public class ClassDiagramCreatedRequestDto {
+}
