f02b1f0d2020098b9ac5688febf9f494966253f1
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-20 14:46:45 +0900

    🚨 [Fix] edit reamde

--- src/main/java/com/server/pnd/domain/ReadMe.java
+++ src/main/java/com/server/pnd/domain/ReadMe.java
@@ -3,6 +3,7 @@ package com.server.pnd.domain;
 import com.server.pnd.util.entity.BaseEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -11,17 +12,21 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "README")
-public class ReadME extends BaseEntity {
+public class ReadMe extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "markdown_id")
-    private Markdown markdown;
+    @ManyToOne
+    @JoinColumn(name = "repo_id")
+    private Repo repo;
 
-    @OneToOne
-    @NotEmpty @JoinColumn(name = "project_id")
-    private Project project;
+    @NotNull
+    private String title;
+
+    private String readme_script;
+
+    private String readme_script_gpt;
 
 }
