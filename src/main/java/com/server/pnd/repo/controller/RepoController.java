b98d5331d7a683a14238c5185aae32c0a51fbdc8
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-28 10:41:43 +0900

    ✨ [Feat] 레포 기본 정보 세팅 - controller

--- src/main/java/com/server/pnd/repo/controller/RepoController.java
+++ src/main/java/com/server/pnd/repo/controller/RepoController.java
@@ -14,24 +14,31 @@ import org.springframework.web.bind.annotation.*;
 public class RepoController {
     private final RepoService repoService;
 
-    // 레포지토리 생성
+    // 레포 생성
     @PostMapping
     public ResponseEntity<CustomApiResponse<?>> createRepo(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody RepoCreatedRequestDto repoCreatedRequestDto) {
         return repoService.createRepo(authorizationHeader, repoCreatedRequestDto);
     }
 
-    // 레포지토리 전체 조회
+    // 레포 전체 조회
     @GetMapping
     public ResponseEntity<CustomApiResponse<?>> searchRepoList(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader){
         return repoService.searchRepoList(authorizationHeader);
     }
 
-    // 레포지토리 상세 조회
+    // 레포 기본 정보 세팅
+    @PutMapping({"/{repo_id}"})
+    public ResponseEntity<CustomApiResponse<?>> settingRepo(
+            @PathVariable("repo_id") Long repoId) {
+        repoService.settingRepo(repoId);
+    }
+    // 레포 상세 조회
     @GetMapping("/{repo_id}")
     public ResponseEntity<CustomApiResponse<?>> searchRepoDetail(
-            @PathVariable("repo_id") Long repoId){
+            @PathVariable("repo_id") Long repoId) {
+
         return repoService.searchRepoDetail(repoId);
     }
 }
