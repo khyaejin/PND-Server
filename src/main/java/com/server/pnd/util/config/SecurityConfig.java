649ca2146c984884ff702a21aa02c7b495943d09
Hjwoon <hjw030705@naver.com>
2024-09-05 17:24:21 +0900

    🚑️ (3) 최신 방식으로 CORS 설정 추가
    1. CorsConfig 파일 내 allowedOriginPatterns 제거, 2. SecurityConfig 파일 내 최신 방식 CORS 설정 방법 업데이트(1. cors(withDefaults() 사용하여 기본 CORS 설정 적용 , CorsConfigurationSource를 사용하여 CORS 설정: CORS 설정을 정의하고, 이를 SecurityFilterChain에 명시적으로 적용하도록 설정)

--- src/main/java/com/server/pnd/util/config/SecurityConfig.java
+++ src/main/java/com/server/pnd/util/config/SecurityConfig.java
@@ -8,6 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -22,8 +29,22 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize
                                 .requestMatchers("/**").permitAll()
-                );
+                )
+                .cors(withDefaults());  // 기본 CORS 설정 적용
         return httpSecurity.build();
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
