78269fe80fd39203d5dd26a1551acea4ff054dc0
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-09-14 08:23:03 +0900

    🚨 [Fix] RepoSettingRequestDto 수정

--- src/main/java/com/server/pnd/repo/dto/RepoSettingRequestDto.java
+++ src/main/java/com/server/pnd/repo/dto/RepoSettingRequestDto.java
@@ -1,13 +1,11 @@
 package com.server.pnd.repo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class RepoSettingRequestDto {
     private String period;
