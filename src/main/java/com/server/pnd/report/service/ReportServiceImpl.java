11b9d573c4cbd50eadc47dab5aa4bf7052a6cbfa
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-09-21 06:12:26 +0900

    ✨ [Feat] index.ts file 접근 위치 수정

--- src/main/java/com/server/pnd/report/service/ReportServiceImpl.java
+++ src/main/java/com/server/pnd/report/service/ReportServiceImpl.java
@@ -71,7 +71,8 @@ public class ReportServiceImpl implements ReportService{
                 scriptPath = "/Users/gimhyejin/Library/CloudStorage/OneDrive-한성대학교/문서/Projects/PND-Server/src/main/resources/scripts/3d-contrib/src/index.ts";
             } else {
                 // Deploy path for EC2 (Linux)
-                scriptPath = "/home/ubuntu/PND-Server/src/main/resources/scripts/3d-contrib/src/index.ts";
+                System.out.println("Current working directory: " + Paths.get("").toAbsolutePath().toString());
+                scriptPath = "../../src/main/resources/scripts/3d-contrib/src/index.ts";
             }
 
             ProcessBuilder processBuilder = new ProcessBuilder("ts-node", scriptPath);
@@ -136,13 +137,14 @@ public class ReportServiceImpl implements ReportService{
                     String outputPath;
                     if (os.contains("win")) {
                         // Windows path
-                        outputPath = "./src/main/resources/scripts/3d-contrib/src/index.ts";
+                        outputPath = "./src/main/resources/profile-3d-contrib/" + svgFileName;
                     } else if (os.contains("mac")) {
                         // macOS path
                         outputPath = "/Users/gimhyejin/Library/CloudStorage/OneDrive-한성대학교/문서/Projects/PND-Server/src/main/resources/profile-3d-contrib/" + svgFileName;
                     } else {
                         // Deploy path for EC2 (Linux)
-                        outputPath = "/home/ubuntu/PND-Server/src/main/resources/profile-3d-contrib/" + svgFileName;
+                        outputPath = "../../src/main/resources/profile-3d-contrib/" + svgFileName;
+
                     }
 
                     File file = new File(outputPath);
--- src/main/resources/scripts/3d-contrib/src/file-writer.ts
+++ src/main/resources/scripts/3d-contrib/src/file-writer.ts
@@ -5,13 +5,13 @@ let OUTPUT_FOLDER: string;
 
 if (os.platform() === 'win32') {
     // Windows path
-    OUTPUT_FOLDER = "./src/main/resources/scripts/3d-contrib/src/index.ts";
+    OUTPUT_FOLDER = './src/main/resources/profile-3d-contrib';
 } else if (os.platform() === 'darwin') {
     // macOS path
     OUTPUT_FOLDER = '/Users/gimhyejin/Library/CloudStorage/OneDrive-한성대학교/문서/Projects/PND-Server/src/main/resources/profile-3d-contrib';
 } else {
     // Deploy path for EC2 (Linux)
-    OUTPUT_FOLDER = '/home/ubuntu/PND-Server/src/main/resources/profile-3d-contrib';
+    OUTPUT_FOLDER = '../../src/main/resources/profile-3d-contrib';
 }
 
 export { OUTPUT_FOLDER };
